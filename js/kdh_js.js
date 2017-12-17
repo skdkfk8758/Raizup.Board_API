9522c69a3ad411e7e0de4e2d8f6db01b04b339dc
KimDongHyun <skdkfk8758@gmail.com>
2017-12-17 22:21:12 +0900

    자바스크립트 리펙토링 , 컨트롤 옵션 추가

--- js/kdh_js.js
+++ js/kdh_js.js
@@ -1,21 +1,8 @@
 
 // 옵션 초기화
-var modeOption = {
-    HORIZONTAL: "horizontal",
-    VERTICAL: "vertical",
-    FADE: "fade"
-};
-
-var pagerTypeOption = {
-    FULL: "full",
-    SHORT: "short"
-};
-
-var boolOptions = {
-    TURE: true,
-    FALSE: false
-};
-
+var modeOption = { HORIZONTAL: "horizontal", VERTICAL: "vertical", FADE: "fade" };
+var pagerTypeOption = { FULL: "full", SHORT: "short" };
+var boolOptions = { TRUE: true, FALSE: false };
 var options = {
   item:
     {
@@ -24,10 +11,10 @@ var options = {
         speed: {default: 500, desc: "슬라이더 속도 설정", type: "input",upperMenu: "general"},
         slideMargin: {default: 0, desc: "슬라이더 이미지 여백 설정", type: "input",upperMenu: "general"},
         startSlide: {default: 0, desc: "시작 슬라이드 설정", type: "input",upperMenu: "general"},
-        randomStart: {selectOption: boolOptions, desc: "시작 슬라이드 랜점 설정", type: "bool_false",upperMenu: "general"},
+        randomStart: {selectOption: boolOptions, desc: "시작 슬라이드 랜덤 설정", type: "bool_false",upperMenu: "general"},
         // slideSelector: ?
         infiniteLoop: {selectOption: boolOptions, desc: "무한반복 설정", type: "bool_true",upperMenu: "general"},
-        hideControlOnEnd: {selectOption: boolOptions, desc: "반복 설정 해제(?)", type: "bool_false",upperMenu: "general"},
+        hideControlOnEnd: {selectOption: boolOptions, desc: "처음 / 끝 슬라이드 도착시 컨트롤러 숨기기(If 무한반복 = false)", type: "bool_false",upperMenu: "general"},
         // easing: null,
         captions: {selectOption: boolOptions, desc: "슬라이드 캡션 설정", type: "bool_false",upperMenu: "general"},
         ticker: {selectOption: boolOptions, desc: "Ticker 설정", type: "bool_false",upperMenu: "general"},
@@ -45,47 +32,46 @@ var options = {
         preventDefaultSwipeY: {selectOption: boolOptions, desc: "터치스크린이 Y 축을 따라 움직이는지 설정", type: "bool_false",upperMenu: "general"},
         // wrapperClass:  -> wrapper 있으면 설정
         // slideWidth: {default: 600, desc: "슬라이드 가로사이즈 설정", type: "input"},
-        
-        //pager옵션
+
+        //pager 옵션
         pager: {selectOption: boolOptions, desc: "페이저 추가", type: "bool_true", upperMenu: "pager"},
         pagerType: {selectOption: pagerTypeOption, desc: "페이저 타입 설정", type: "pagerType", upperMenu: "pager"},
-        pagerShortSeparator: {default: '/', desc: "페이저 구분자 설정", type: "input", upperMenu: "pager"},
-        pagerSelector: {default: '', desc: "페이저 Selector", type: "input", upperMenu: "pager"},
-        pagerCustom: {default: '', desc: "페이저 커스터마이징", type: "input", upperMenu: "pager"},
-        buildPager: {default: '', desc: "페이저 ?", type: "input", upperMenu: "pager"}
+        pagerShortSeparator: {default: '/', desc: "페이저 구분자 설정(If PagerType = Short)", type: "input", upperMenu: "pager"},
+        pagerSelector: {default: '', desc: "호출기를 채우는데 사용된 요소", type: "input", upperMenu: "pager"},
+        pagerCustom: {default: '', desc: "페이저로 사용될 선택자징", type: "input", upperMenu: "pager"},
+        buildPager: {default: '', desc: "슬라이드에서 함수가 호출되고 호출기항목은 마크업으로 사용 -> 함수지정", type: "input", upperMenu: "pager"},
+
+        //controls 옵션
+        controls : {selectOption: boolOptions, desc: "컨트롤러 추가", type: "bool_true", upperMenu: "controls"},
+        nextText : {default: "Next", desc: "Next 컨트롤에 사용할 수 있는 텍스트", type: "input", upperMenu: "controls"},
+        prevText : {default: "Prev", desc: "Prev 컨트롤에 사용할 수 있는 텍스트", type: "input", upperMenu: "controls"},
+        nextSelector : {default: "", desc: "Next 컨트롤에 사용할 수 있는 선택자", type: "input", upperMenu: "controls"},
+        prevSelector : {default: "", desc: "Prev 컨트롤에 사용할 수 있는 선택자", type: "input", upperMenu: "controls"},
+        autoControls : {selectOption: boolOptions, desc: "Start / Stop 컨트롤러 추가", type: "bool_false", upperMenu: "controls"},
+        startText : {default: "Start", desc: "Start 컨트롤에 사용할 수 있는 텍스트", type: "input", upperMenu: "controls"},
+        stopText : {default: "Stop", desc: "Stop 컨트롤에 사용할 수 있는 텍스트", type: "input", upperMenu: "controls"},
+        autoControlsCombine : {selectOption: boolOptions, desc: "슬라이드 동작시 Stop 표시", type: "bool_false", upperMenu: "controls"},
+        autoControlsSelector : {selectOption: boolOptions, desc: "자동 컨트롤을 채우는데 사용되는 선택자", type: "bool_true", upperMenu: "controls"},
+        keyboardEnabled : {selectOption: boolOptions, desc: "키보트 컨트롤 설정", type: "bool_false", upperMenu: "controls"},
     }
 };
-    
-function showTable(){
-     $.each(options.item, function(name, value) {
-                buildTableRow(name, value);
-        });
-}
-    
+
+// ----------------------------  테이블 생성
+function showTable(){ $.each(options.item, function(name, value) { buildTableRow(name, value); } ); }
 function buildTableRow(name, value){
     switch (value.type) {
-        case 'mode':
-            buildSelectBox(name, value, value.selectOption.HORIZONTAL, value.upperMenu);
-            break;
-        case 'input':
-            buildInputBox(name, value, value.upperMenu);
-            break;
-        case 'bool_false':
-            buildSelectBox(name, value, value.selectOption.FALSE, value.upperMenu);
-            break;
-        case 'bool_true':
-            buildSelectBox(name, value, value.selectOption.TURE, value.upperMenu);
-            break;
-        case 'pagerType':
-            buildSelectBox(name, value, value.selectOption.FULL, value.upperMenu);
-            break;
-        
+        case 'mode': buildSelectBox(name, value, value.selectOption.HORIZONTAL, value.upperMenu); break;
+        case 'input': buildInputBox(name, value, value.upperMenu); break;
+        case 'bool_false': buildSelectBox(name, value, value.selectOption.FALSE, value.upperMenu); break;
+        case 'bool_true': buildSelectBox(name, value, value.selectOption.TRUE, value.upperMenu); break;
+        case 'pagerType': buildSelectBox(name, value, value.selectOption.FULL, value.upperMenu); break;
         default:
             // code
     }
-            
 }
+//---------------------------------------
 
+// SelectBox 구성
 function buildSelectBox(name, value, defaultOption, upperMenu){
     var newTableRow = `
                 <tr>
@@ -99,30 +85,25 @@ function buildSelectBox(name, value, defaultOption, upperMenu){
                 </tr>
                 `;
     $(`#${upperMenu}-option-table`).append(newTableRow);
-    
-    $.each(value.selectOption, function(index, value){
-        if(value != defaultOption){
-            var selectOption = `<option value="${value}">${value}</option>`;
-            // alert(name + " " + selectOption);
-            $(`#${name}`).append(selectOption);
-        }
-    });
+
+    $.each(value.selectOption, function(index, value){ if(value != defaultOption){ $(`#${name}`).append(`<option value="${value}">${value}</option>`); } } );
 }
 
+// InputBox 구성
 function buildInputBox(name, value, upperMenu){
     var newTableRow = `
                 <tr>
                     <td>Set ${name}</td>
                     <td>${value.desc}</td>
                     <td>
-                        <input type="text" id="${name}" value="${value.default}">  
+                        <input type="text" id="${name}" value="${value.default}">
                     </td>
                 </tr>
                 `;
      $(`#${upperMenu}-option-table`).append(newTableRow);
 }
 
-
+// ---------------------------- 적용버튼 실행
 $("#apply-button").click(function() {
     changeSlider();
 });
@@ -132,10 +113,84 @@ function changeSlider(){
     $('.slider').bxSlider(setOptions());
     $('.setting-form').hide();
 }
+// ------------------------------------------
+
+//  일반옵션
+function setMode(){ options.mode = $("#mode option:selected").val(); }
+function setSpeed(){ options.speed = parseInt($("#speed").val()); }
+function setMargin(){ options.slideMargin = parseInt($("#slideMargin").val()); }
+function setStartSlide(){ options.startSlide = parseInt($("#startSlide").val())-1; }
+function setRandomStart(){  var value = $("#randomStart option:selected").val(); (value == 'true') ? options.randomStart = value : options.randomStart = ""; }
+function setLoop(){ var value = $("#infiniteLoop option:selected").val(); (value == 'true') ? options.infiniteLoop = value : options.infiniteLoop = ""; }
+function setHideController(){ var value = $("#hideControlOnEnd option:selected").val(); (value  == 'true') ? options.hideControlOnEnd = value : options.hideControlOnEnd = ""; }
+function setCaptions(){ var value = $("#captions option:selected").val(); (value  == 'true') ? options.captions = value : options.captions = ""; }
+function setTicker(){ var value = $("#ticker option:selected").val(); (value  == 'true') ? options.ticker = value : options.ticker = ""; }
+function setTickerHover(){ var value = $("#tickerHover option:selected").val(); (value  == 'true') ? options.tickerHover = value : options.tickerHover = ""; }
+function setAdaptiveHeight(){ var value = $("#adaptiveHeight option:selected").val(); (value  == 'true') ? options.adaptiveHeight = value : options.adaptiveHeight = ""; }
+function setAdaptiveHeightSpeed(){ options.adaptiveHeightSpeed = parseInt($("#adaptiveHeightSpeed").val()); }
+function setVideo(){ var value = $("#video option:selected").val(); (value  == 'true') ? options.video = value : options.video = ""; }
+function setResponse(){ var value = $("#responsive option:selected").val(); (value  == 'true') ? options.responsive = value : options.responsive = ""; }
+function setUseCSS(){ var value = $("#useCSS option:selected").val(); (value  == 'true') ? options.useCSS = value : options.useCSS = ""; }
+function setTouchEnabled(){ var value = $("#touchEnabled option:selected").val(); (value  == 'true') ? options.touchEnabled = value : options.touchEnabled = ""; }
+function setSwipeThreshold(){ options.swipeThreshold = parseInt($("#swipeThreshold").val()); }
+function setOneToOneTouch(){ var value = $("#oneToOneTouch option:selected").val(); (value == 'true') ? options.oneToOneTouch = value : options.oneToOneTouch = ""; }
+function setPreventDefaultSwipeX(){ var value = $("#preventDefaultSwipeX option:selected").val(); (value == 'true') ? options.preventDefaultSwipeX = value : options.preventDefaultSwipeX = ""; }
+function setPreventDefaultSwipeY(){ var value = $("#preventDefaultSwipeY option:selected").val(); (value == 'true') ? options.preventDefaultSwipeY = value : options.preventDefaultSwipeY = ""; }
+function setSlideWidth(){ options.slideWidth = parseInt($("#slideWidth").val()); }
+
+// Pager 옵션
+function setPager(){ var value = $("#pager option:selected").val(); (value == 'true') ? options.pager = value : options.pager = ""; }
+function setPagerType(){ options.pagerType = $("#pagerType option:selected").val(); }
+function setPagerShortSeparator(){ options.pagerShortSeparator = $("#pagerShortSeparator").val(); }
+function setPagerSelector(){ options.pagerSelector = $("#pagerSelector").val(); }
+function setPagerCustom(){ options.pagerCustom = $("#pagerCustom").val(); }
+function setBuildPager(){ options.buildPager = $("#buildPager").val(); }
+
+// Controls 옵션
+function setControls() { var value = $("#controls option:selected").val(); (value == 'true') ? options.controls = value : options.controls = ""; }
+function setNextText(){ options.nextText = $("#nextText").val(); }
+function setPrevText(){ options.prevText = $("#prevText").val(); }
+function setNextSelector(){ options.nextSelector = $("#nextSelector").val(); }
+function setPrevSelector(){ options.prevSelector = $("#prevSelector").val(); }
+function setAutoControls() { var value = $("#autoControls option:selected").val(); (value == 'true') ? options.autoControls = value : options.autoControls = ""; }
+function setStartText(){ options.startText = $("#startText").val(); }
+function setStopText(){ options.stopText = $("#stopText").val(); }
+function setAutoControlsCombine() { var value = $("#autoControlsCombine option:selected").val(); (value == 'true') ? options.autoControlsCombine = value : options.autoControlsCombine = ""; }
+function setAutoControlsSelector() { var value = $("#autoControlsSelector option:selected").val(); (value == 'true') ? options.autoControlsSelector = value : options.autoControlsSelector = ""; }
+function setKeyboardEnabled() { var value = $("#keyboardEnabled option:selected").val(); (value == 'true') ? options.keyboardEnabled = value : options.keyboardEnabled = ""; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 뷰에서 설정된 옵션 세팅
 function setOptions() {
-    setMode(); 
+    setMode();
     setSpeed();
     setMargin();
     setStartSlide();
@@ -156,7 +211,7 @@ function setOptions() {
     setPreventDefaultSwipeX();
     setPreventDefaultSwipeY();
     setSlideWidth();
-    
+
     setPager();
     setPagerType();
     setPagerCustom();
@@ -164,191 +219,17 @@ function setOptions() {
     setPagerShortSeparator();
     setBuildPager();
 
+    setControls();
+    setNextText();
+    setPrevText();
+    setNextSelector();
+    setPrevSelector();
+    setAutoControls();
+    setStartText();
+    setStopText();
+    setAutoControlsCombine();
+    setAutoControlsSelector();
+    setKeyboardEnabled();
+
     return options;
 }
-
-//  일반옵션
-
-function setMode(){
-    var value = $("#mode option:selected").val();
-    options.mode = value;
-}
-
-function setSpeed(){
-    var value = $("#speed");
-    options.speed = parseInt(value.val());
-}
-
-function setMargin(){
-    var value = $("#slideMargin");
-    options.slideMargin = parseInt(value.val());
-}
-
-function setStartSlide(){
-    var value = $("#startSlide");
-    options.startSlide = parseInt(value.val())-1;
-}
-
-function setRandomStart(){
-    var value = $("#randomStart option:selected").val();
-    if(value == 'true')
-        options.randomStart = value;
-    else
-        options.randomStart = "";
-}
-
-function setLoop(){
-    var value = $("#infiniteLoop option:selected").val();
-    if(value == 'true')
-        options.infiniteLoop = value;
-    else
-        options.infiniteLoop = "";
-}
-
-function setHideController(){
-    var value = $("#hideControlOnEnd option:selected").val();
-     if(value == 'true')
-        options.hideControlOnEnd = value;
-    else
-        options.hideControlOnEnd = "";
-}
-
-function setCaptions(){
-    var value = $("#captions option:selected").val();
-    if(value == 'true')
-        options.captions = value;
-    else
-        options.captions = "";
-}
-
-function setTicker(){
-    var value = $("#ticker option:selected").val();
-    if(value == 'true')
-        options.ticker = value;
-    else
-        options.ticker = "";
-}
-
-function setTickerHover(){
-    var value = $("#tickerHover option:selected").val();
-    if(value == 'true')
-        options.tickerHover = value;
-    else
-        options.tickerHover = "";
-}
-
-function setAdaptiveHeight(){
-    var value = $("#adaptiveHeight option:selected").val();
-   if(value == 'true')
-        options.adaptiveHeight = value;
-    else
-        options.adaptiveHeight = "";
-}
-
-function setAdaptiveHeightSpeed(){
-    var value = $("#adaptiveHeightSpeed");
-    options.adaptiveHeightSpeed = parseInt(value.val());
-}
-
-function setVideo(){
-    var value = $("#video option:selected").val();
-    if(value == 'true')
-        options.video = value;
-    else
-        options.video = "";
-}
-
-function setResponse(){
-    var value = $("#responsive option:selected").val();
-    if(value == 'true')
-        options.responsive = value;
-    else
-        options.responsive = "";
-}
-
-function setUseCSS(){
-    var value = $("#useCSS option:selected").val();
-    if(value == 'true')
-        options.useCSS = value;
-    else
-        options.useCSS = "";
-}
-
-function setTouchEnabled(){
-    var value = $("#touchEnabled option:selected").val();
-    if(value == 'true')
-        options.touchEnabled = value;
-    else
-        options.touchEnabled = "";
-}
-
-function setSwipeThreshold(){
-    var value = $("#swipeThreshold");
-    options.swipeThreshold = parseInt(value.val());
-}
-
-function setOneToOneTouch(){
-    var value = $("#oneToOneTouch option:selected").val();
-    if(value == 'true')
-        options.oneToOneTouch = value;
-    else
-        options.oneToOneTouch = "";
-}
-
-function setPreventDefaultSwipeX(){
-    var value = $("#preventDefaultSwipeX option:selected").val();
-    if(value == 'true')
-        options.preventDefaultSwipeX = value;
-    else
-        options.preventDefaultSwipeX = "";
-}
-
-function setPreventDefaultSwipeY(){
-    var value = $("#preventDefaultSwipeY option:selected").val();
-    if(value == 'true')
-        options.preventDefaultSwipeY = value;
-    else
-        options.preventDefaultSwipeY = "";
-}
-
-function setSlideWidth(){
-    var value = $("#slideWidth");
-    options.slideWidth = parseInt(value.val());
-}
-
-// Pager 옵션
-
-function setPager(){
-    var value = $("#pager option:selected").val();
-    if(value == 'true')
-        options.pager = value;
-    else
-        options.pager = "";
-}
-
-function setPagerType(){
-    var value = $("#pagerType option:selected").val();
-    options.pagerType = value;
-}
-
-function setPagerShortSeparator(){
-    var value = $("#pagerShortSeparator");
-    options.pagerShortSeparator = value.val();
-}
-
-function setPagerSelector(){
-    var value = $("#pagerSelector");
-    options.pagerSelector = value.val();
-}
-
-function setPagerCustom(){
-    var value = $("#pagerCustom");
-    options.pagerCustom = value.val();
-}
-
-function setBuildPager(){
-    var value = $("#buildPager");
-    options.buildPager = value.val();
-}
-
-
--- skin/latest/shop_basic/latest.skin.php
+++ skin/latest/shop_basic/latest.skin.php
@@ -1,24 +1,24 @@
 <?php
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
-include_once(G5_LIB_PATH.'/latest.lib.php');
-include_once('./_common.php');
+    if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+    include_once(G5_LIB_PATH.'/latest.lib.php');
+    include_once('./_common.php');
 
-// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
-add_javascript('<script src="'.G5_JS_URL.'/jquery.bxslider.js"></script>', 10);
-add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0);
+    // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+    add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
+    add_javascript('<script src="'.G5_JS_URL.'/jquery.bxslider.js"></script>', 10);
+    add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0);
 ?>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
-    
+
     <!-- 합쳐지고 최소화된 최신 CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    
+
     <!-- 부가적인 테마 -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-    
+
     <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
@@ -37,26 +37,29 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
 
     <div class="setting-form">
         <ul id="myTab" class="nav nav-tabs" role="tablist">
-            <li role="presentation" class="active"><a href="#general_option" id="general-tab" role="tab" data-toggle="tab" aria-controls="general_option" aria-expanded="false">일반옵션</a></li>
+            <li role="presentation" class="active"><a href="#general_option" id="general-tab" role="tab" data-toggle="tab" aria-controls="general_option" aria-expanded="true">일반옵션</a></li>
             <li role="presentation" class=""><a href="#pager_option" role="tab" id="pager-tab" data-toggle="tab" aria-controls="pager_option" aria-expanded="true">Pager옵션</a></li>
+            <li role="presentation" class=""><a href="#controls_option" role="tab" id="controls-tab" data-toggle="tab" aria-controls="controls_option" aria-expanded="true">Control옵션</a></li>
         </ul>
+
         <div id="myTabContent" class="tab-content">
-         <div role="tabpanel" class="tab-pane fade" id="general_option" aria-labelledby="general-tab">
-             <table id="general-option-table">
-                <tr>
-                    <th>옵션</th><th>설명</th><th>선택</th>
-                </tr>
-             </table>
+            <div role="tabpanel" class="tab-pane fade active in" id="general_option" aria-labelledby="general-tab">
+                <table id="general-option-table">
+                    <tr> <th>옵션</th><th>설명</th><th>선택</th> </tr>
+                </table>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="pager_option" aria-labelledby="pager-tab">
+                <table id="pager-option-table">
+                    <tr> <th>옵션</th><th>설명</th><th>선택</th> </tr>
+                </table>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="controls_option" aria-labelledby="pager-tab">
+                <table id="controls-option-table">
+                    <tr> <th>옵션</th><th>설명</th><th>선택</th> </tr>
+                </table>
+            </div>
+
         </div>
-        <div role="tabpanel" class="tab-pane fade" id="pager_option" aria-labelledby="pager-tab">
-             <table id="pager-option-table">
-                <tr>
-                    <th>옵션</th><th>설명</th><th>선택</th>
-                </tr>
-             </table>
-        </div>
-        
-        </div>  
 
         <button id="apply-button">적용</button>
 
@@ -68,4 +71,3 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
             });
         </script>
     </div>
-    
